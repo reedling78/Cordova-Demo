6e37bb4a9ea15a0736fac005f173a20e8def13fd
Reed Rizzo <reedling78@gmail.com>
2016-10-19 16:27:43 -0500

    Added require and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
+var gulp = require('gulp');
+var amdOptimize = require('amd-optimize');
+var concat = require('gulp-concat');
+
+gulp.task('bundle-js', function () {
+    'use strict';
+
+    return gulp.src('www/js/main.js')
+
+        .pipe(amdOptimize('main', {
+            name: "main",
+            configFile: "www/js/main.js",
+            baseUrl: 'www/js/'
+        }))
+
+        .pipe(concat('main.js'))
+
+        .pipe(gulp.dest('www/min'));
 });
+
+gulp.task('default', ['bundle-js']);
---
+++ www/js/main.js
@@ -0,0 +1,15 @@
+/*jslint nomen: true*/
+/*globals window, document, define, $, _, localStorage, dataLayer, console, Modernizr, Fonts, alert*/
+require.config({
+    deps: ['main'],
+    paths: {
+        'jquery': '../bower_components/jquery/dist/jquery.min',
+        'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min'
+    },
+    shim: { 'bootstrap': { deps: ['jquery'] } }
+});
+
+define(['bootstrap'], function () {
+    'use strict';
+    console.log('whatever');
+});
