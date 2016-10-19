a075247ea8f73c0716838c8a5a394a04a0bcdbac
Reed Rizzo <reedling78@gmail.com>
2016-10-19 16:50:04 -0500

    Added bootstrap-sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@
 var gulp = require('gulp');
 var amdOptimize = require('amd-optimize');
 var concat = require('gulp-concat');
+var sass = require('gulp-sass');
 
 gulp.task('bundle-js', function () {
     'use strict';
@@ -16,6 +17,13 @@ gulp.task('bundle-js', function () {
 
         .pipe(concat('main.js'))
 
+        .pipe(gulp.dest('www/min'));
+});
+
+gulp.task('sass', function () {
+    'use strict';
+    return gulp.src('www/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('www/min'));
 });
 
--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,11 @@
 /*jslint nomen: true*/
 /*globals window, document, define, $, _, localStorage, dataLayer, console, Modernizr, Fonts, alert*/
+
 require.config({
     deps: ['main'],
     paths: {
         'jquery': '../bower_components/jquery/dist/jquery.min',
-        'bootstrap': '../bower_components/bootstrap/dist/js/bootstrap.min'
+        'bootstrap': '../bower_components/bootstrap-sass/assets/javascripts/bootstrap.min'
     },
     shim: { 'bootstrap': { deps: ['jquery'] } }
 });
